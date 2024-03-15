680b019863f2c5e4aaa4ae53e8866afb8e36333a
FloreDenis2001 <floredenis907@yahoo.com>
2024-03-15 14:01:17 +0200

    last_update

--- src/App.tsx
+++ src/App.tsx
@@ -296,6 +296,7 @@ function App() {
           </div>
           <div className="card__content">
             <h3>Titlul Industrie</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas sint id totam est explicabo distinctio dolorem dolorum eligendi nam? Et, maiores cum quae eum nesciunt unde atque! Consectetur, modi ipsam!</p>
           </div>
         </div>
         <div className="industry__card">
@@ -340,7 +341,36 @@ function App() {
         </div>
        
       
-       
+        <div className="card__collection clear-fix">
+<div className="cards cards--two">
+    <img src="https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d702cb99ca804bffcfa8820c46483264&auto=format&fit=crop&w=651&q=80" className="img-responsive" alt="Cards Image"/>
+    <span className="cards--two__rect"></span>
+    <span className="cards--two__tri"></span>
+    <p>Lucy Grace</p>
+    <ul className="cards__list">
+      <li><i className="fab fa-facebook-f"></i></li>
+      <li><i className="fab fa-twitter"></i></li>
+      <li><i className="fab fa-instagram"></i></li>
+      <li><i className="fab fa-linkedin-in"></i></li>
+    </ul>
+  </div>
+  <div className="cards cards--three">
+    <img src="https://images.unsplash.com/photo-1480408144303-d874c5e12201?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=575213599ae24b3dbdfd84be79425c50&auto=format&fit=crop&w=634&q=80" className="img-responsive" alt=""/>
+    <span className="cards--three__rect-1">
+      <span className="shadow-1"></span>
+      <p>Chris Levnon</p>
+    </span>
+    <span className="cards--three__rect-2">
+      <span className="shadow-2"></span>
+    </span>
+    <span className="cards--three__circle"></span>
+    <ul className="cards--three__list">
+      <li><i className="fab fa-facebook-f"></i></li>
+      <li><i className="fab fa-twitter"></i></li>
+      <li><i className="fab fa-linkedin-in"></i></li>
+    </ul>
+  </div>
+</div>
       </div>
     </section>
     </>
